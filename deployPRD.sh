993adb6242b35bb42d0fbbfe3168b1a1e5c652bb
Ammar Amara <ammaramara785@gmail.com>
2026-06-04 17:28:42 +0300

    Added smart backup directory creation with conditions

--- deployPRD.sh
+++ deployPRD.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+echo "====================================="
+echo "Starting Automated Production Deployment..."
+echo "====================================="
 
-echo "=========================================="
-echo "🚀 Starting Automated Production Deployment..."
-echo "=========================================="
+echo "Deployer: $USER"
+echo "Date: $(date)"
 
-echo "👤 Deployer: $USER"
-echo "📅 Date: $(date)"
-
-echo "------------------------------------------"
-echo "🔍 Checking System Storage..."
-echo "------------------------------------------"
-
-# أمر لعرض مساحة القرص الصلب بشكل منظم وسهل القراءة
+echo "-------------------------------------"
+echo "Checking System Storage..."
+echo "-------------------------------------"
 df -h --total | grep total
 
-echo "=========================================="
-echo "🎯 Application Deployed Successfully!"
-echo "=========================================="
+echo "-------------------------------------"
+echo "Setting Up Backup Directory..."
+echo "-------------------------------------"
+
+BACKUP_DIR="app_backups"
+
+if [ ! -d "$BACKUP_DIR" ]; then
+mkdir "$BACKUP_DIR" 
+echo "Directory '$BACKUP_DIR' created successfully."
+else
+echo "Directory '$BACKUP_DIR' already exists. Skipping creation."
+fi
+echo "====================================="
+echo "Application Deployed Successfully!"
