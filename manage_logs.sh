485fb92c2e1d70e22f1319169ee46aad3fa696c2
Ammar Amara <ammaramara785@gmail.com>
2026-06-08 19:29:06 +0300

    feat: added automated log managment and rotation script

---
+++ manage_logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "==============================================="
+echo "[+] Starting Log Managemnt & Rotation..."
+echo "==============================================="
+
+LOG_FILE="app.log"
+BACKUP_DIR="app_backups"
+
+MAX_SIZE=10240
+
+CURRENT_SIZE=$(wc -c < "$LOG_FILE")
+
+echo "Current Log Size: $CURRENT_SIZE Bytes"
+
+if [ "$CURRENT_SIZE" -gt "$MAX_SIZE" ]; then
+echo "[WARNING] Log file exceeded $MAX_SIZE bytes! Rotating..."
+
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+BACKUP_FILE="$BACKUP_DIR/app_log_$TIMESTAMP.log"
+
+cp "$LOG_FILE" "$BACKUP_FILE"
+> "$LOG_FILE"
+
+echo "[SUCCESS]Old logs backed up to $BACKUP_FILE and app.log cleared!"
+else
+echo "[OK] log file size is within the safe limit."
+fi
+
